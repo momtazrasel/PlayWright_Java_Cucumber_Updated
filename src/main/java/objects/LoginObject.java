fc04929b175cba61e5bf188a1b759c476351c59c
momtazrasel <momtazrasel8@gmail.com>
2025-01-12 23:58:51 +0600

    All file are updated

--- src/main/java/objects/LoginObject.java
+++ src/main/java/objects/LoginObject.java
@@ -6,4 +6,5 @@ public class LoginObject {
     public static final String PASSWORD_INPUT = "(//input[@id='Password'])[1]";
     public static final String LOGIN_BUTTON = "(//input[@value='Log in'])[1]";
     public static final String LOGIN_ERROR = "(//div[@class='validation-summary-errors'])[1]";
+//    public static final String LOGIN_ERROR = "(//div[@class='validation-summary-errors'])[1]";
 }
